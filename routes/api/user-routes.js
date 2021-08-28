065298853f0673d5b211d7966b78c0fa79b4dda3
JakeClark33 <jakemclark@hotmail.com>
2021-08-28 16:35:10 -0500

    hello

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -46,12 +46,18 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+router.post('/login', (req, res) => {
+
+  // Query operation
+
+})
 
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
@@ -87,5 +93,30 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      // res.json({ user: dbUserData });
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+      // Verify user
+  
+    });  
+  });
 
 module.exports = router;
