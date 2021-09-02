ca18ee44d7bb18d120588f9c4383a2c78d49c71e
JakeClark33 <jakemclark@hotmail.com>
2021-09-01 22:12:51 -0500

    html

--- controller/index.js
+++ controller/index.js
@@ -4,7 +4,8 @@ const apiRoutes = require('./api');
 
 const homeRoutes = require('./home-routes.js');
 
-router.use('/', homeRoutes);
+
+router.use('/api', homeRoutes);
 
 router.use('/api', apiRoutes);
 
