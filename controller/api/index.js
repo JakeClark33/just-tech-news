35356ddce1b813d0f2fcdc374136bd31d0f9d7fd
JakeClark33 <jakemclark@hotmail.com>
2021-09-02 21:00:39 -0500

    javascript

--- controller/api/index.js
+++ controller/api/index.js
@@ -3,9 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-
+const loginRoutes = require('./javascript/login.js');
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
-
+router.use('/login', loginRoutes);
 module.exports = router;
