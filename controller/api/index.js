74be8e583abd40cc67e1ad9de9bb29cc2263c786
JakeClark33 <jakemclark@hotmail.com>
2021-09-02 21:27:52 -0500

    14.3

--- controller/api/index.js
+++ controller/api/index.js
@@ -3,9 +3,24 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-const loginRoutes = require('./javascript/login.js');
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
-router.use('/login', loginRoutes);
 module.exports = router;
--- controller/home-routes.js
+++ controller/home-routes.js
@@ -4,6 +4,7 @@ const { Post, User, Comment } = require('../models');
 
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
       attributes: [
         'id',
@@ -40,8 +41,15 @@ router.get('/', (req, res) => {
   });
   router.get('/login', (req, res) => {
     res.render('login');
-  });router.get('/login', (req, res) => {
-  res.render('login');
-});
+  });
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
   module.exports = router;
